df5a2307f003b864a9280425b804b7946c477e43
Dheeresha <dheeresha.s@sigmainfo.net>
2022-12-22 16:02:53 +0530

    Code updates

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,7 +6,7 @@ class Invoice < ApplicationRecord
 
   validates :is_paid, :customer_id, :total, presence: true
 
-  after_update :update_invoice_batch, if: -> { saved_change_amount? }
+  after_update :update_invoice_batch, if: -> { saved_change_total? }
 
   private
 
--- config/initializers/db_listener.rb
+++ config/initializers/db_listener.rb
@@ -1,14 +1,16 @@
-ActiveRecord::Base.connection_pool.with_connection do |connection|
-  conn = connection.instance_variable_get(:@connection)
+# ActiveRecord::Base.connection_pool.with_connection do |connection|
+#   begin
+#     connection.execute 'LISTEN invoices_changed'
 
-  begin
-    conn.async_exec 'LISTEN invoices_changed'
+#     connection.raw_connection.wait_for_notify do |channel, _pid, payload|
+#       Rails.logger.info "Received a NOTIFY on channel #{channel}"
+#       InvoiceBatchNotifierJob.perform_now(payload)
+#     end
+#   ensure
+#     connection.execute 'UNLISTEN *'
+#   end
+# end
 
-    conn.wait_for_notify do |channel, _pid, payload|
-      Rails.logger.info "Received a NOTIFY on channel #{channel}"
-      InvoiceBatchNotifierJob.perform_now(payload)
-    end
-  ensure
-    conn.async_exec 'UNLISTEN *'
-  end
-end
+# def connection
+#   ActiveRecord::Base.connection
+# end
